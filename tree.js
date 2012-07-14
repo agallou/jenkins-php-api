aa6bc73a84c216e0f89fbd07673235368758fe46
Adrien Gallou <adriengallou@gmail.com>
2012-07-14 12:33:06 +0000

    Mise à jour gh-pages

--- tree.js
+++ tree.js
@@ -1,6 +1,5 @@
 var tree = [["[Global Namespace]",,"",
-                [["Jenkins","Jenkins.html","",
-                [        ]],["Jenkins_Autoloader","Jenkins_Autoloader.html","",
+                [["Jenkins_Autoloader","Jenkins_Autoloader.html","",
                 [        ]],["Jenkins_Build","Jenkins_Build.html","",
                 [        ]],["Jenkins_Computer","Jenkins_Computer.html","",
                 [        ]],["Jenkins_Executor","Jenkins_Executor.html","",
@@ -8,7 +7,6 @@ var tree = [["[Global Namespace]",,"",
                 [        ]],["Jenkins_Job","Jenkins_Job.html","",
                 [        ]],["Jenkins_JobQueue","Jenkins_JobQueue.html","",
                 [        ]],["Jenkins_Queue","Jenkins_Queue.html","",
-                [        ]],["Jenkins_TestReport","Jenkins_TestReport.html","",
                 [        ]],["Jenkins_View","Jenkins_View.html","",
                 [        ]]        ]]        ]
 
